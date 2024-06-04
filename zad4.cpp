6c3519a224e821b6277498f1d7c6adc1ba199c43
JamesSevil <jsclassified24@gmail.com>
2024-06-04 15:21:01 +0700

    Задание 4

--- zad4.cpp
+++ zad4.cpp
@@ -17,134 +17,73 @@ bool notrepeat(int round_number, vector<bool> self_choices, vector<bool> enemy_c
     else return true;
 }
 
+void play(int rounds, string a_choices, string b_choices, int& score_a, int& score_b) {
+    vector<bool> self_choices; // Свои решения
+    vector<bool> enemy_choices; // решения противника
+    for (int i = 0; i < rounds; i++) {
+        bool a_choice, b_choice;
+        if (a_choices == "Сотрудничает") {
+            a_choice = sotr(i, self_choices, enemy_choices);
+        } else if (a_choices == "Предаёт") {
+            a_choice = pred(i, self_choices, enemy_choices);
+        } else if (a_choices == "Не повторяет") {
+            a_choice = notrepeat(i, self_choices, enemy_choices);
+        }
+        b_choice = notrepeat(i, enemy_choices, self_choices);
+
+        if (a_choice == true && b_choice == true) { // оба сотрудничают
+            self_choices.push_back(true);
+            enemy_choices.push_back(true);
+        } else if (a_choice == false && b_choice == false) { // оба предают
+            self_choices.push_back(false);
+            enemy_choices.push_back(false);
+        } else if (a_choice == true && b_choice == false) { // a сотрудничает, b предает
+            self_choices.push_back(true);
+            enemy_choices.push_back(false);
+        } else { // a предает, b сотрудничает
+            self_choices.push_back(false);
+            enemy_choices.push_back(true);
+        }
+    }
+    for (int i = 0; i < rounds; i++) {
+        if (self_choices[i] == true && enemy_choices[i] == true) {
+            score_a += 24;
+            score_b += 24;
+        } else if (self_choices[i] == false && enemy_choices[i] == false) {
+            score_a += 4;
+            score_b += 4;
+        } else if (self_choices[i] == true && enemy_choices[i] == false) {
+            score_a += 0;
+            score_b += 20;
+        } else {
+            score_a += 20;
+            score_b += 0;
+        }
+    }
+
+}
+
+
 int main() {
     setlocale(LC_ALL, "Rus");
     srand(time(0));
 
     // Игра 1(Мы всегда сотрудничаем, противник всегда не повторяет)
     int rounds = 100 + rand() % 101;
-    vector<bool> self_choices; // Свои решения
-    vector<bool> enemy_choices; // решения противника
-    for (int i = 0; i < rounds; i++) {
-        bool a_choice = sotr(i, self_choices, enemy_choices);
-        bool b_choice = notrepeat(i, enemy_choices, self_choices);
-
-        if (a_choice == true && b_choice == true) { // оба сотрудничают
-            self_choices.push_back(true);
-            enemy_choices.push_back(true);
-        } else if (a_choice == false && b_choice == false) { // оба предают
-            self_choices.push_back(false);
-            enemy_choices.push_back(false);
-        } else if (a_choice == true && b_choice == false) { // a сотрудничает, b предает
-            self_choices.push_back(true);
-            enemy_choices.push_back(false);
-        } else { // a предает, b сотрудничает
-            self_choices.push_back(false);
-            enemy_choices.push_back(true);
-        }
-    }
-    // Подсчет очков
-    int score_a = 0;
-    int score_b = 0;
-    for (int i = 0; i < rounds; i++) {
-        if (self_choices[i] == true && enemy_choices[i] == true) {
-            score_a += 24;
-            score_b += 24;
-        } else if (self_choices[i] == false && enemy_choices[i] == false) {
-            score_a += 4;
-            score_b += 4;
-        } else if (self_choices[i] == true && enemy_choices[i] == false) {
-            score_a += 0;
-            score_b += 20;
-        } else {
-            score_a += 20;
-            score_b += 0;
-        }
-    }
-    cout << "Игра 1.\nКол-во раундов: " << rounds << "\nАлгоритм A: " << score_a << "\nАлгоритм B: " << score_b << endl;
+    int score_a = 0, score_b = 0;
+    play(rounds, "Сотрудничает", "Не повторяет", score_a, score_b);
+    cout << "Игра 1.\nКол-во раундов: " << rounds << "\nАлгоритм A: " << score_a << "\nАлгоритм B: " << score_b << endl << endl;
 
     // Игра 2(Мы всегда предаём, противник всегда не повторяет)
     rounds = 100 + rand() % 101;
-    self_choices.clear(); // Свои решения
-    enemy_choices.clear(); // решения противника
-    for (int i = 0; i < rounds; i++) {
-        bool a_choice = pred(i, self_choices, enemy_choices);
-        bool b_choice = notrepeat(i, enemy_choices, self_choices);
-
-        if (a_choice == true && b_choice == true) { // оба сотрудничают
-            self_choices.push_back(true);
-            enemy_choices.push_back(true);
-        } else if (a_choice == false && b_choice == false) { // оба предают
-            self_choices.push_back(false);
-            enemy_choices.push_back(false);
-        } else if (a_choice == true && b_choice == false) { // a сотрудничает, b предает
-            self_choices.push_back(true);
-            enemy_choices.push_back(false);
-        } else { // a предает, b сотрудничает
-            self_choices.push_back(false);
-            enemy_choices.push_back(true);
-        }
-    }
-    // Подсчет очков
-    score_a = 0;
-    score_b = 0;
-    for (int i = 0; i < rounds; i++) {
-        if (self_choices[i] == true && enemy_choices[i] == true) {
-            score_a += 24;
-            score_b += 24;
-        } else if (self_choices[i] == false && enemy_choices[i] == false) {
-            score_a += 4;
-            score_b += 4;
-        } else if (self_choices[i] == true && enemy_choices[i] == false) {
-            score_a += 0;
-            score_b += 20;
-        } else {
-            score_a += 20;
-            score_b += 0;
-        }
-    }
-    cout << "Игра 2.\nКол-во раундов: " << rounds << "\nАлгоритм A: " << score_a << "\nАлгоритм B: " << score_b << endl;
+    score_a = 0, score_b = 0;
+    play(rounds, "Предаёт", "Не повторяет", score_a, score_b);
+    cout << "Игра 2.\nКол-во раундов: " << rounds << "\nАлгоритм A: " << score_a << "\nАлгоритм B: " << score_b << endl << endl;
 
     // Игра 3(Мы всегда не повторяем, противник всегда не повторяет)
     rounds = 100 + rand() % 101;
-    self_choices.clear(); // Свои решения
-    enemy_choices.clear(); // решения противника
-    for (int i = 0; i < rounds; i++) {
-        bool a_choice = notrepeat(i, self_choices, enemy_choices);
-        bool b_choice = notrepeat(i, enemy_choices, self_choices);
-
-        if (a_choice == true && b_choice == true) { // оба сотрудничают
-            self_choices.push_back(true);
-            enemy_choices.push_back(true);
-        } else if (a_choice == false && b_choice == false) { // оба предают
-            self_choices.push_back(false);
-            enemy_choices.push_back(false);
-        } else if (a_choice == true && b_choice == false) { // a сотрудничает, b предает
-            self_choices.push_back(true);
-            enemy_choices.push_back(false);
-        } else { // a предает, b сотрудничает
-            self_choices.push_back(false);
-            enemy_choices.push_back(true);
-        }
-    }
-    // Подсчет очков
-    score_a = 0;
-    score_b = 0;
-    for (int i = 0; i < rounds; i++) {
-        if (self_choices[i] == true && enemy_choices[i] == true) {
-            score_a += 24;
-            score_b += 24;
-        } else if (self_choices[i] == false && enemy_choices[i] == false) {
-            score_a += 4;
-            score_b += 4;
-        } else if (self_choices[i] == true && enemy_choices[i] == false) {
-            score_a += 0;
-            score_b += 20;
-        } else {
-            score_a += 20;
-            score_b += 0;
-        }
-    }
+    score_a = 0, score_b = 0;
+    play(rounds, "Не повторяет", "Не повторяет", score_a, score_b);
     cout << "Игра 3.\nКол-во раундов: " << rounds << "\nАлгоритм A: " << score_a << "\nАлгоритм B: " << score_b << endl;   
 
     return 0;
